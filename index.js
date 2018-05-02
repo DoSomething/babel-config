8edca5a5c46b528b06aba20ebe68fbec3120f2fa
David Furnes <david@dfurnes.com>
2018-05-02 12:09:07 -0400

    Set our supported browsers as Babel targets.

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const runningTests = process.env.NODE_ENV === 'test';
 module.exports = {
   presets: [
     [require('babel-preset-env'), {
+      // Set our supported browsers. <goo.gl/w43BMg>
+      targets: {
+        browsers: ['>0.5%', 'ie 11', 'not op_mini all'],
+      },
       // Jest needs CommonJS modules to run in Node. If building for
       // Webpack 3, don't compile modules so we can use scope hoisting.
       modules: runningTests ? 'commonjs' : false,
