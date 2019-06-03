981606d196b289e5096937c2f84a79d40e7314cf
David Furnes <david@dfurnes.com>
2019-06-03 14:07:41 -0400

    Add 'withGraphQL' option for optimization opt-out.

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-module.exports = (api, { withPolyfills = true }) => {
+module.exports = (api, { withGraphQL = true, withPolyfills = true }) => {
   const runningTests = process.env.NODE_ENV === 'test';
-  const polyfillSettings = withPolyfills ? {
-    useBuiltIns: withPolyfills ? 'usage' : false,
-    corejs: 3,
-  } : null;
+  const polyfillSettings = withPolyfills
+    ? {
+        useBuiltIns: withPolyfills ? 'usage' : false,
+        corejs: 3,
+      }
+    : null;
+
+  const optionalPlugins = [];
+  if (withGraphQL) {
+    optionalPlugins.push(require('babel-plugin-graphql-tag'));
+  }
 
   return {
     presets: [
-      [require('@babel/preset-env'), {
-        // Set our supported browsers. <goo.gl/w43BMg>
-        targets: {
-          browsers: ['>0.5%', 'ie 11', 'not op_mini all'],
+      [
+        require('@babel/preset-env'),
+        {
+          // Set our supported browsers. <goo.gl/w43BMg>
+          targets: {
+            browsers: ['>0.5%', 'ie 11', 'not op_mini all'],
+          },
+
+          // Jest needs CommonJS modules to run in Node. If building for
+          // Webpack 3, don't compile modules so we can use scope hoisting.
+          modules: runningTests ? 'commonjs' : false,
+
+          // If enabled, bundle 'core-js' polyfills for any features
+          // that we use into the build.
+          ...polyfillSettings,
         },
-
-        // Jest needs CommonJS modules to run in Node. If building for
-        // Webpack 3, don't compile modules so we can use scope hoisting.
-        modules: runningTests ? 'commonjs' : false,
-
-        // If enabled, bundle 'core-js' polyfills for any features
-        // that we use into the build.
-        ...polyfillSettings
-      }],
+      ],
       require('@babel/preset-react'),
     ],
     plugins: [
@@ -30,8 +40,8 @@ module.exports = (api, { withPolyfills = true }) => {
       require('@babel/plugin-syntax-dynamic-import'),
       require('@babel/plugin-proposal-export-default-from'),
       require('@babel/plugin-proposal-export-namespace-from'),
-      require('babel-plugin-graphql-tag'),
       require('babel-plugin-lodash'),
+      ...optionalPlugins,
     ],
     env: {
       production: {
@@ -41,10 +51,8 @@ module.exports = (api, { withPolyfills = true }) => {
         ],
       },
       test: {
-        plugins: [
-          require('babel-plugin-dynamic-import-node'),
-        ],
+        plugins: [require('babel-plugin-dynamic-import-node')],
       },
     },
-  }
+  };
 };
